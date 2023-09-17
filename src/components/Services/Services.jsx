b8723e99b3f87e63da212c658d51bd7670716320
Caesar <ushmanp@gmail.com>
2023-09-17 14:33:21 +0100

    olasystem

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -11,16 +11,20 @@ const Services = () => {
 
         <div className="grid">
             <div className="gird1">
-                <h1>IT Staffing</h1>
-                <h1>IT Staffing</h1>
-                <h1>IT Staffing</h1>
-                <h1>IT Staffing</h1>
-                <h1>IT Staffing</h1>
-                <h1>IT Staffing</h1>
-                <h1>IT Staffing</h1>
-                <h1>IT Staffing</h1>
-                <h1>IT Staffing</h1>
+                <div className="staff">
+                 <h1>IT Staffing</h1>
+                </div>
+
+                <h1>API Integration</h1>
+                <h1>Banking & Finance</h1>
+                <h1>IT Consultations</h1>
+                <h1>Micro Service Development</h1>
                 <img src={olasystem} alt="Olasystem" />
+                <h1>Web Development</h1>
+                <h1>Mobile Development</h1>
+                <h1>Databse Administrator</h1>
+                <h1>and many more</h1>
+                
             </div>
         </div>
       </div>
